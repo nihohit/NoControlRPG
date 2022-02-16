65d99fba5291fbd6efb51eec117d75a2cf11bf55
Shachar Langbeheim <nihohit@gmail.com>
2022-02-16 13:14:03 +0200

    Fix double hits.

--- Assets/Scripts/ShotScript.cs
+++ Assets/Scripts/ShotScript.cs
@@ -8,11 +8,14 @@ public class ShotScript : MonoBehaviour {
 
   private BattleMainManagerScript manager;
 
+  private bool alreadyHit;
+
   public void Init(int layer, Vector3 startPoint, WeaponConfig config, BattleMainManagerScript manager) {
     gameObject.layer = layer;
     StartPoint = startPoint;
     Config = config;
     this.manager = manager;
+    alreadyHit = false;
   }
 
   public bool InRange() {
@@ -20,6 +23,10 @@ public class ShotScript : MonoBehaviour {
   }
 
   private void OnTriggerEnter2D(Collider2D other) {
+    if (alreadyHit) {
+      return;
+    }
+    alreadyHit = true;
     var enemy = other.gameObject.GetComponent<EnemyUnitScript>();
     if (enemy != null) {
       manager.ShotHit(this, enemy);
