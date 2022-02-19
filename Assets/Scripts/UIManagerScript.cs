4e77a53d0ffb22518f02219b02ca2ff2a8c29303
Shachar Langbeheim <nihohit@gmail.com>
2022-02-20 00:16:33 +0200

    Initial inventory UI split.

--- Assets/Scripts/UIManagerScript.cs
+++ Assets/Scripts/UIManagerScript.cs
@@ -7,19 +7,23 @@ public class UIManagerScript : MonoBehaviour {
   private TMPro.TMP_Text switchContextText;
 
   private BattleMainManagerScript mainManager;
+  private GameObject inventoryUIHolder;
 
   // Start is called before the first frame update
   void Awake() {
     switchContextText = transform.Find("SwitchContext").transform.Find("Text").GetComponent<TMPro.TMP_Text>();
     mainManager = GameObject.Find("Manager").GetComponent<BattleMainManagerScript>();
+    inventoryUIHolder = transform.Find("inventory").gameObject;
   }
 
   public void ToInventoryMode() {
     switchContextText.text = "Launch to battle";
+    inventoryUIHolder.SetActive(true);
   }
 
   public void ToBattleMode() {
     switchContextText.text = "Return to Base";
+    inventoryUIHolder.SetActive(false);
   }
 
   public void SwitchContextPressed() {
