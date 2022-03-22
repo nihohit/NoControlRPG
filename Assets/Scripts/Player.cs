9cbc2ed335f1715fd8acc75d2bad95670f735962
Shachar Langbeheim <nihohit@gmail.com>
2022-03-22 13:28:49 +0200

    Change Equipment to be a read only collection.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,19 +1,21 @@
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Assets.Scripts.Base;
 
 public class Player {
-  public List<WeaponInstance> Weapons { set; get; }
-  public List<WeaponInstance> AvailableItems { set; get; }
+  public ReadOnlyCollection<WeaponInstance> Weapons { private set; get; }
+  public ReadOnlyCollection<WeaponInstance> AvailableItems { private set; get; }
 
   public static readonly Player Instance = new() {
     Weapons = new List<WeaponInstance>{
       new WeaponInstance(WeaponConfig.MACHINE_GUN),
       new WeaponInstance(WeaponConfig.TWO_SHOT_SHOTGUN)
-    },
+    }.ToReadOnlyCollection(),
     AvailableItems = new List<WeaponInstance>{
       new WeaponInstance(WeaponConfig.MISSILE),
       new WeaponInstance(WeaponConfig.FLAMER),
       new WeaponInstance(WeaponConfig.LASER),
       new WeaponInstance(WeaponConfig.RIFLE)
-    }
+    }.ToReadOnlyCollection()
   };
 }
